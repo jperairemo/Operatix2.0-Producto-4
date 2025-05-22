173719fc8c65e1b16d4027082edff5ca3c5b121a
GayLachner-Marc <mgayl@uoc.edu>
2025-05-22 18:03:49 +0200

    Visual changes

---
+++ Producto3(FP.064)_Operatix2.0/Producto3(FP.064)_Operatix2.0/Producto3/Producto3/wp_data/wp-content/themes/operatixtheme/patterns/redes-sociales.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Title: Redes Sociales
+ * Slug: operatixtheme/redes-sociales
+ * Categories: 
+ */
+?>
+<!-- wp:group {"metadata":{"categories":["footer"],"patternName":"core/pie-de-pagina-centrado-con-enlaces-sociales","name":"Pie de página centrado con enlaces sociales"},"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","bottom":"var:preset|spacing|60","left":"var:preset|spacing|50","top":"var:preset|spacing|60"},"blockGap":"var:preset|spacing|40","margin":{"top":"0","bottom":"0"}},"dimensions":{"minHeight":"40vh"}},"textColor":"contrast","layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
+<div class="wp-block-group alignfull has-contrast-color has-text-color" style="min-height:40vh;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--50)"><!-- wp:site-logo {"align":"center","style":{"spacing":{"margin":{"bottom":"6px"}}}} /-->
+
+<!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
+<p class="has-text-align-center has-medium-font-size"><?php esc_html_e('¡Síguenos en nuestras redes sociales para mantenerte al día de las novedades!', 'operatixtheme');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"size":"has-normal-icon-size","className":"is-style-logos-only","style":{"spacing":{"blockGap":{"top":"12px","left":"12px"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<ul class="wp-block-social-links has-normal-icon-size is-style-logos-only"><!-- wp:social-link {"url":"","service":"facebook"} /-->
+
+<!-- wp:social-link {"service":"instagram"} /-->
+
+<!-- wp:social-link {"service":"linkedin"} /-->
+
+<!-- wp:social-link {"service":"youtube"} /-->
+
+<!-- wp:social-link {"service":"google"} /--></ul>
+<!-- /wp:social-links -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('copyright@', 'operatixtheme');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Política de protección de datos', 'operatixtheme');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Att al cliente: 93 202 23 17', 'operatixtheme');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
